d1c32b26b1c74d8c446a1cb24b2b6538c103f433
夜千殤 <Yeh-Hsiu-Hsiang@users.noreply.github.com>
2016-12-14 19:41:04 +0800

    Update README.py

--- README.py
+++ README.py
@@ -1,3 +1,3 @@
-import tkinter
-win=tkinter.Tk()
-win.mainloop()
+Python 
+  Tkinter introduce : https://read01.com/4Dzx3a.html
+GUI 視窗建立: http://yhhuang1966.blogspot.tw/2016/05/python-gui-tkinter.html
